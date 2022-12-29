667ff546bf08d6dd17848aedc4ad34a4280ea01b
Anrtyer <120300933+Anrtyer@users.noreply.github.com>
2022-12-29 11:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://Anrtyer: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@github.com/Anrtyer/myrp okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://Anrtyer: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@github.com/Anrtyer/myrp okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m pyUltroid &")
